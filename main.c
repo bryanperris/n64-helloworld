2cc993ac91f15678e599cca25cfd72dcedb9dbc2
bperris <bryanperris@live.com>
2019-05-07 14:32:49 -0400

    turn on some graphical features

--- main.c
+++ main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+#include <libdragon.h>
 
-int main()
+int main(void)
 {
+   init_interrupts();
+   display_init(RESOLUTION_320x240, DEPTH_32_BPP, 1, GAMMA_NONE, ANTIALIAS_OFF);
+   console_init();
+   console_set_render_mode(RENDER_MANUAL);
+   console_clear();
    printf("Hello, World!");
-   return 0;
+   console_render();
 }

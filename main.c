145526725123af1531d7885a41efa7ba7c8d7bc8
Bryan Perris <bryanperris@live.com>
2019-05-07 22:04:00 -0400

    some fixes, works in pj64

--- main.c
+++ main.c
@@ -4,10 +4,10 @@
 int main(void)
 {
    init_interrupts();
-   display_init(RESOLUTION_320x240, DEPTH_32_BPP, 1, GAMMA_NONE, ANTIALIAS_OFF);
+   display_init(RESOLUTION_320x240, DEPTH_32_BPP, 2, GAMMA_NONE, ANTIALIAS_OFF);
    console_init();
    console_set_render_mode(RENDER_MANUAL);
    console_clear();
-   printf("Hello, World!");
+   printf("Hello, World!\n");
    console_render();
 }
